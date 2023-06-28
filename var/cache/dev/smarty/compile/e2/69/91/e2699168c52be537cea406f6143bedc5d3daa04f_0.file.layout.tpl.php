8b01c29c9ebd4dae6f483747215e1857739d9697
Marianne THOMAS <m.thomas1403@gmail.com>
2023-06-28 16:43:39 +0200

    ajout de mymodule.zip et de blocages.txt

---
+++ var/cache/dev/smarty/compile/e2/69/91/e2699168c52be537cea406f6143bedc5d3daa04f_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-06-28 15:45:14
+  from 'C:\xampp\htdocs\prestashop\adminMt\themes\default\template\controllers\login\layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_649c396ab20313_39600555',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e2699168c52be537cea406f6143bedc5d3daa04f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prestashop\\adminMt\\themes\\default\\template\\controllers\\login\\layout.tpl',
+      1 => 1681819498,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_649c396ab20313_39600555 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
